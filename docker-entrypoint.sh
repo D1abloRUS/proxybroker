0b61cbb8c9e97ffb45ab547f6b6889223c3ee5d3
Sergey Monakhov <monakhov@ordbuy.com>
2017-03-17 13:13:35 +0300

    fix cleanup file

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 if [ $# -eq 0 ]; then
+  cat /dev/null > proxies.txt
   proxybroker grab --limit 1000 | sed -nr 's/.*\[\] (.*)>.*/\1/p' >> proxies.txt
   go-proxycheck -treds=${JOBS} -url=${SITE} -in=proxies.txt
 else
